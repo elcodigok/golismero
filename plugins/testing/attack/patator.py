30156014330468cfce8e164090e8d4205b649a18
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2014-03-22 17:43:54 -0300

    Portscan potator for SSH

--- plugins/testing/attack/patator.py
+++ plugins/testing/attack/patator.py
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import shlex
 from time import time
+from os.path import join
 
 from golismero.api.config import Config
 from golismero.api.data.resource.domain import Domain
@@ -37,12 +38,12 @@ from golismero.api.data.resource.url import URL
 from golismero.api.data.resource.ip import IP
 from golismero.api.data.information.fingerprint import ServiceFingerprint
 from golismero.api.data.information.portscan import Portscan
-from golismero.api.external import run_external_tool, tempfile, \
-    find_binary_in_path, get_tools_folder
+from golismero.api.external import run_external_tool, tempfile, find_binary_in_path, get_tools_folder
 from golismero.api.logger import Logger
 from golismero.api.net import ConnectionSlot
 from golismero.api.plugin import TestingPlugin
 from golismero.api.data import Relationship
+from golismero.api.text.wordlist import WordListLoader
 
 
 #------------------------------------------------------------------------------
@@ -64,6 +65,11 @@ class PatatorPlugin(TestingPlugin):
 
     #--------------------------------------------------------------------------
     def run(self, info):
+
+        # Get user args
+        #user_args = shlex.split(Config.plugin_args["args"])
+        path_password = WordListLoader.get_wordlist(Config.plugin_args["password"])
+
         if info[0].is_instance(IP):
             Logger.log(info[0])
             Logger.log("Es una instancia de IP")
@@ -75,7 +81,30 @@ class PatatorPlugin(TestingPlugin):
         if info[1].is_instance(ServiceFingerprint):
             Logger.log("Es una instancia de ServiceFingerprint")
             Logger.log(info[1])
-            pass # hacer algo con el servicio
         elif info[1].is_instance(Portscan):
             Logger.log("Es un Portscan")
-            Logger.log(info[1])
+            #Logger.log(info[1].ports)
+            #Logger.log(info[1].address)
+            for service in info[1].ports:
+                Logger.log(service[2])
+                if service[2] == 22:
+                    args = [
+                        "ssh_login",
+                        "host=%s" % (info[0]), 
+                        "port=22",
+                        "user=root",
+                        "password=FILE0",
+                        "0=%s" % ("wordlist/" + Config.plugin_args["password"])
+                    ]
+                    Logger.log(args)
+                    Logger.log("Patator against: %s" % info[1].address)
+                    Logger.log_more_verbose("Patator arguments: %s" % " ".join(args))
+ 
+                    patator_script = join(get_tools_folder(), "patator", "patator_v0.5.py")
+ 
+                    with ConnectionSlot(info[1].address):
+                        t1 = time()
+                        code = run_external_tool(patator_script, args, callback=Logger.log_verbose)
+                        t2 = time()
+
+        return
